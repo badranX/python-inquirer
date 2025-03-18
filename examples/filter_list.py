ab07fdeaa777642bcc1a72a1b27a23daff51e2f9
Yahya Badran <yahya.badran@gmail.com>
2025-03-18 04:40:09 -0500

    add FilterList autocomplete example

--- examples/filter_list.py
+++ examples/filter_list.py
@@ -11,6 +11,17 @@ if 'hint' in args:
 else:
     choices_hints = None
 
+if 'autocomplete' in args:
+    def autocomplete_func(_text, state):
+        # Every time the user presses TAB, we'll switch to the next suggestion
+        # The `state` variable contains the index of the current suggestion
+        # We can wrap it around to the first suggestion if we reach the end
+        # The suggestion are the filtered choices in this example
+        l = list(filter_func(_text, choices))
+        return l[state % len(l)] if l else _text
+else:
+    autocomplete_func = None
+
 carousel =  True if 'carousel' in args else False
 other = True if 'other' in args else False
 
@@ -40,6 +51,7 @@ questions = [
         other=other,
         hints= choices_hints,
         filter_func=filter_func,
+        autocomplete=autocomplete_func,
         choice_callback=callback_listener,
     ),
 ]
